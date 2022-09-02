1536608f46b0c27f1e3693012a8b1ee03df4cc8b
LazyDuckling <emi@lazyduckling.dev>
2022-09-02 07:08:03 -0500

    Fixed issues in regex for email validation where special characters were allowed where they shouldn't have been

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
+// Grab classes from HTML
 const formInputs = document.querySelectorAll(".form__input");
 const formErrors = document.querySelectorAll(".form__error");
 const formSuccess = document.querySelectorAll(".form__success");
+
+// Variables for repeated values
+const errorIconShow = "url('./images/icon-error.svg')";
+const errorIconHide = "url('')";
+const displayBlock = "block";
+const displayNone = "none"
+
 
 // Add event listeners to the input fields and tie the updateInput function to them
 function listenInputs() {
@@ -12,17 +20,17 @@ function listenInputs() {
 listenInputs();
 
 // Regular expression to make sure email address is valid
-const emailRegExp = /(?:[\u0000-\u0FFF][^@\s]+)@{1}\w+\.\w+$/u;
+const emailRegExp = /[^.]+[\u0000-\u0FFF]+[^.@]@{1}[a-zA-Z0-9]+.{1}[a-zA-Z]+$/g;
 let emailVerification = emailRegExp.exec(formInputs[2].value);
 
 // Checks if the input has text in it - Display error message if everything is erased and remove when something is typed
 function updateInput(e) {
     if (e.target.value != "") {
-        e.target.nextElementSibling.style.display = "none";
-        e.target.style.backgroundImage = "url('')";
+        e.target.nextElementSibling.style.display = displayNone;
+        e.target.style.backgroundImage = errorIconHide;
     } else {
-        e.target.nextElementSibling.style.display = "block";
-        e.target.style.backgroundImage = "url('./images/icon-error.svg')";
+        e.target.nextElementSibling.style.display = displayBlock;
+        e.target.style.backgroundImage = errorIconShow;
     }
 
     // Check if the email entered matches the regular expression
@@ -36,6 +44,7 @@ function updateInput(e) {
 
 // Converts the strings into boolean to check if there is a value in the input
 function verifyForm() {
+    // Variable to check if all inputs are valid on submission
     let inputValid = [
         false,
         false,
@@ -49,13 +58,13 @@ function verifyForm() {
         // If the input is empty, display error elements and the background image in inputs
         // If it has content, remove/do not display those elements
         if (inputNotEmpty != true) {
-            formErrors[i].style.display = "block";
-            formInputs[i].style.backgroundImage = "url('./images/icon-error.svg')";
+            formErrors[i].style.display = displayBlock;
+            formInputs[i].style.backgroundImage = errorIconShow;
 
             inputValid[i] = false;
         } else {
-            formErrors[i].style.display = "none";
-            formInputs[i].style.backgroundImage = "url('')";
+            formErrors[i].style.display = displayNone;
+            formInputs[i].style.backgroundImage = errorIconHide;
 
             inputValid[i] = true;
         }
@@ -63,19 +72,19 @@ function verifyForm() {
 
     // Check if email is valid with regular expression on submit, display appropriate error message
     if (Boolean(formInputs[2].value) == true && emailVerification == null) {
-        formErrors[2].style.display = "block";
-        formInputs[2].style.backgroundImage = "url('./images/icon-error.svg')";
+        formErrors[2].style.display = displayBlock;
+        formInputs[2].style.backgroundImage = errorIconShow;
         formErrors[2].innerHTML = "Looks like this is not a valid Email Address";
-    } else if (Boolean(formInputs[2].value) == false && emailVerification == null){
-        formErrors[2].style.display = "block";
-        formInputs[2].style.backgroundImage = "url('./images/icon-error.svg')";
+    } else if (Boolean(formInputs[2].value) == false && emailVerification == null) {
+        formErrors[2].style.display = displayBlock;
+        formInputs[2].style.backgroundImage = errorIconShow;
         formErrors[2].innerHTML = "Email Address cannot be empty";
     }
 
     // Show success message if all inputs are valid
     if (inputValid.every(Boolean) && emailVerification != null) {
-        formSuccess[0].style.display = "block";
+        formSuccess[0].style.display = displayBlock;
     } else {
-        formSuccess[0].style.display = "none";
+        formSuccess[0].style.display = displayNone;
     }
 }
